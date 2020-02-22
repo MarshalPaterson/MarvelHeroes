7257dba24c420b4f9897ba81b3138fa8ae912759
Marshal Paterson <MARSHAL.PATERSON@star.com.au>
2020-02-22 21:15:50 +1100

    Working on a FlatList with mock data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,55 @@
-import React, { Component } from 'react'
-import {
-    StyleSheet,
-    Text,
-    View
-} from 'react-native'
-
-interface Props {
-
-}
-
-interface State {
-
-}
-
-export default class App extends Component<Props, State> {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.text}>
-                    Welcome to Marvel Heroes!
-                </Text>
-            </View>
-        );
-    }
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    text: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-});
+import React, { Component } from 'react';  
+import { AppRegistry, FlatList,  
+    StyleSheet, Text, View,Alert } from 'react-native';  
+  
+export default class App extends Component {  
+  
+    renderSeparator = () => {  
+        return (  
+            <View  
+                style={{  
+                    height: 1,  
+                    width: "100%",  
+                    backgroundColor: "#000",  
+                }}  
+            />  
+        );  
+    };  
+    //handling onPress action  
+    getListViewItem = (item) => {  
+        Alert.alert(item.key);  
+    }  
+  
+    render() {  
+        return (  
+            <View style={styles.container}>  
+                <FlatList  
+                    data={[  
+                        {key: 'Android'},{key: 'iOS'}, {key: 'Java'},{key: 'Swift'},  
+                        {key: 'Php'},{key: 'Hadoop'},{key: 'Sap'},  
+                        {key: 'Python'},{key: 'Ajax'}, {key: 'C++'},  
+                        {key: 'Ruby'},{key: 'Rails'},{key: '.Net'},  
+                        {key: 'Perl'},{key: 'Sap'},{key: 'Python'},  
+                        {key: 'Ajax'}, {key: 'C++'},{key: 'Ruby'},  
+                        {key: 'Rails'},{key: '.Net'},{key: 'Perl'}  
+                    ]}  
+                    renderItem={({item}) =>  
+                        <Text style={styles.item}  
+                              onPress={this.getListViewItem.bind(this, item)}>{item.key}</Text>}  
+                    ItemSeparatorComponent={this.renderSeparator}  
+                />  
+            </View>  
+        );  
+    }  
+}  
+  
+const styles = StyleSheet.create({  
+    container: {  
+        flex: 1,  
+    },  
+    item: {  
+        padding: 10,  
+        fontSize: 18,  
+        height: 44,  
+    },  
+})  
